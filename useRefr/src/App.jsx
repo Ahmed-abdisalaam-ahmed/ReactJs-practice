4e8487197b0cc08efae1d6393c4bf49c0102019b
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2025-11-01 20:28:23 +0300

    finished

---
+++ useRefr/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Count from './Counter.jsx'
+// import inputRef from './inputRef.jsx'
+
+const App = () => {
+  return (
+    <Count/>
+  )
+}
+
+export default App
