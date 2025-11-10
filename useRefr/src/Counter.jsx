8dce439c26c54259a96e87b072bb5cf3adf20df1
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2025-11-10 07:46:32 +0300

    finished

--- useRefr/src/Counter.jsx
+++ useRefr/src/Counter.jsx
@@ -1,24 +1,9 @@
-import React,{useRef,useEffect} from 'react'
+import React,{} from 'react'
 
 const Counter = () => {
-  // mutable(useRef) waa marka aad rabto in aad keydiso value aan dib u renderin component-ka , iyo inaan qiimaha(value) ga directirey  uga badali karo
-  // unmutable(useState)waa marka automatic dib u renderin component-ka kadib markaa value ga la badalo , si aan uga badalo aray cusub ayuu sameyn iyo qiima cusub
-  
-    const inputRef = useRef(null);
-
-    useEffect(()=>{
-      if(inputRef.current){
-        inputRef.current.focus();
-      }
-    },[])
-
-    console.log(inputRef);
-
-
   return (
     <div>
-        <h1>this Onfocus using useRef</h1>
-        <input ref={inputRef} type="text" placeholder='Enter your name' />
+      <h1>Counter Component</h1>
     </div>
   )
 }
--- useRefr/src/InputRef.jsx
+++ useRefr/src/InputRef.jsx
@@ -1,13 +1,19 @@
 import { useRef, useEffect } from 'react'
 
 const InputRef = () => {
-  const inputRef = useRef(null)
 
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus()
-    }
-  }, [])
+    // mutable(useRef) waa marka aad rabto in aad keydiso value aan dib u renderin component-ka , iyo inaan qiimaha(value) ga directirey  uga badali karo
+  // unmutable(useState)waa marka automatic dib u renderin component-ka kadib markaa value ga la badalo , si aan uga badalo aray cusub ayuu sameyn iyo qiima cusub
+  
+    const inputRef = useRef(null);
+
+    useEffect(()=>{
+      if(inputRef.current){
+        inputRef.current.focus();
+      }
+    },[])
+
+    console.log(inputRef);
 
   return (
     <div>
