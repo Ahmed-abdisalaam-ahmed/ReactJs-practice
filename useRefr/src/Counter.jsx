c17da68eee33216722f44b4d458eb9946c086b12
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2025-11-10 09:58:29 +0300

    finished

--- useRefr/src/Counter.jsx
+++ useRefr/src/Counter.jsx
@@ -1,9 +1,24 @@
-import React,{} from 'react'
+import React,{ useEffect, useState , useRef} from 'react'
 
 const Counter = () => {
+
+  const [count,setCount] = useState(0);
+
+  const prevCounter = useRef(0)
+
+  useEffect(()=>{
+    prevCounter.current = count
+  },[count])
+
+  console.log("Previous Count:", prevCounter.current);
+
+
   return (
     <div>
       <h1>Counter Component</h1>
+      <h2>Current: {count}</h2>
+      <p>PrevCurrent: {prevCounter.current}</p>
+      <button onClick={()=> setCount(count + 1)}>Increament</button>
     </div>
   )
 }
