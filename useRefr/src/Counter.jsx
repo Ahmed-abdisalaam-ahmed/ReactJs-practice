2a228d2b36ac10f9da77f99a8b3a8e601275d0e6
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2025-11-10 07:44:51 +0300

    finished j

--- useRefr/src/Counter.jsx
+++ useRefr/src/Counter.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
 
 const Counter = () => {
+  // mutable(useRef) waa marka aad rabto in aad keydiso value aan dib u renderin component-ka , iyo inaan qiimaha(value) ga directirey  uga badali karo
+  // unmutable(useState)waa marka automatic dib u renderin component-ka kadib markaa value ga la badalo , si aan uga badalo aray cusub ayuu sameyn iyo qiima cusub
+  
+    const inputRef = useRef(null);
+
+    useEffect(()=>{
+      if(inputRef.current){
+        inputRef.current.focus();
+      }
+    },[])
+
+    console.log(inputRef);
+
+
   return (
     <div>
-        <h1>
-          
-        </h1>
+        <h1>this Onfocus using useRef</h1>
+        <input ref={inputRef} type="text" placeholder='Enter your name' />
     </div>
   )
 }
