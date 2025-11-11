325e92fd2a5eb9480fb70d69b2e107e42dc382d6
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2025-11-11 20:43:38 +0300

    finished the Dom

--- useRefr/src/Dom.jsx
+++ useRefr/src/Dom.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useRef } from 'react'
+import './index.css'
 
 const Dom = () => {
+
+    const cardRef = useRef(null);
+
+    const handleToggleHighlight = () => {
+        if(cardRef.current){
+            cardRef.current.classList.toggle('highlight');
+        }
+    }
+
+
   return (
-    <div>Dom</div>
+    <div style={{textAlign:'center'}}>
+        <div  ref={cardRef}  className='card'>
+            <h2>Interactive Card</h2>
+            <p>Clic the button to toggle the highlights</p>
+        </div>
+        <button onClick={handleToggleHighlight}>
+            Toggle Highlight
+        </button>
+    </div>
   )
 }
 
