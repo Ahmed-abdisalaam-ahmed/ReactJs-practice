ba997060fad5682706644dd30d319d357e29b0fa
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2025-12-02 17:44:32 +0300

    finished

--- ReactRouter/components/NotFound.jsx
+++ ReactRouter/components/NotFound.jsx
@@ -9,9 +9,9 @@ const NotFound = () => {
 
 
   return (
-    <div>
+    <div >
       <Nav/>
-      <h1 className="text-3xl font-bold text-red-500">{error.status} ---- {error.statusText}-! </h1>
+      <h1 className="text-3xl font-bold text-red-500"> {error.status} ---- {error.statusText}-! </h1>
       <p className="text-xl mt-4">The page you are looking for does not exist.</p>
       </div>
   )
